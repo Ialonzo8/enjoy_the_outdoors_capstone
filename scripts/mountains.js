dd6fe4478b5d73bad6d61d3c1d800137f63cc14d
Ivan Alonzo <ivanalonzo84@gmail.com>
2024-05-28 00:51:09 -0500

    outdoors capstone complete

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,16 +1,133 @@
 "use strict"
+/*
+bootstrap card example:
 
+<div class="card">
+  <img src="PET IMAGE PATH GOES HERE" class="card-img-top" alt="ALT TEXT HERE">
+  <div class="card-body">
+    <h5 class="card-title">PET NAME HERE</h5>
+    <p class="card-text">PET DETAILS HERE</p>
+  </div>
+</div>
+*/
 
 window.onload = () => {
 
-    /*
-    You can remove the following console.log() lines.
-    They are here to verify that we have access to the data
-    The data script files are located in the scripts/data directory
-*/
+    initMountainDropDown();
 
-    //log the mountainsArray to the console (scripts/data/mountainData.js)
-    console.log(mountainsArray)
+    let mountainSelect = document.querySelector("#mountainInfo");
+
+    mountainSelect.addEventListener("change", displayMountainCard)
+
+}
+function initMountainDropDown() {
+
+    let mountainSelect = document.querySelector("#mountainInfo");
+
+    let defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.textContent = "Select A Mountain";
+    mountainSelect.appendChild(defaultOption);
+
+    //write a loop to work with each individual category and build an option for it
+    mountainsArray.forEach((mount) => {
+
+        //create the new option for the category we are on in the loop
+        let newOption = document.createElement("option");
+
+        //set the value for the option
+        newOption.value = mount.name;
+
+        //set what the user sees 
+        newOption.textContent = mount.name;
+
+        mountainSelect.appendChild(newOption);
+
+
+    })
+
+}
+function displayMountainCard(event) {
+
+    console.log(event.target.value)
+    let mountainCards = document.querySelector("#mountainCards")
+    let noMountain = document.querySelector("#noMountain")
+    // this if test will bring the message with ID nothing back to the div when no mountainInfo is selected and vise versa
+    if (event.target.value == 0) {
+        noMountain.removeAttribute("style")
+        mountainCards.setAttribute("style", "display: none")
+
+    } else {
+      mountainCards.removeAttribute("style")
+      noMountain.setAttribute("style", "display: none")
+    }
+
+    //This si the empty DIV in the HTML to display the card generated in here
+    let mountainDiv = document.querySelector("#mountainCards");
+
+    mountainDiv.innerHTML = "";
+
+    //get selected value of the dropdown
+    let selectedMountain = event.target.value;
+
+    let mountain = mountainsArray.find((mountain) => {
+
+        if (mountain.name === selectedMountain) {
+            return true;
+        }
+        return false;
+
+    });
+
+
+ let wholeCard = document.createElement("div");
+ wholeCard.classList.add("card", "text-center", "w-25", "mx-auto","mt-4");
+
+ let cardHeader = document.createElement("div");
+ cardHeader.classList.add("card-header", "bg-dark", "text-light");
+
+ cardHeader.innerText = mountain.name;
+
+ let cardBody = document.createElement("div")
+ cardBody.classList.add("card-body","bg-dark", "text-light");
+
+ let cardImage = document.createElement("img");
+ cardImage.classList.add("card-img-top","card-img-fit");
+
+
+ cardImage.src = `images/${mountain.img}`;
+
+
+    let cardText = document.createElement("p");
+   cardText.classList.add("card-text");
+   cardText.innerText = `${mountain.desc}`
+
+cardBody.appendChild(cardImage);
+cardBody.appendChild(cardText);
+
+let div1= document.createElement("div")
+div1.classList.add("card-footer", "bg-light");
+
+div1.innerText = `Elevation : ${mountain.elevation}`;
+
+let div2= document.createElement("div")
+div2.classList.add("card-footer", "bg-light");
+
+div2.innerText = `Effort : ${mountain.effort}`;
+
+let div3= document.createElement("div")
+div3.classList.add("card-footer", "bg-light");
+
+div3.innerText = `Lat : ${mountain.coords.lat} Lng : ${mountain.coords.lng}`;
+
+
+//ppend all this to make the card
+wholeCard.appendChild(cardHeader);
+wholeCard.appendChild(cardBody);
+wholeCard.appendChild(div1);
+wholeCard.appendChild(div2);
+wholeCard.appendChild(div3);
+mountainDiv.appendChild(wholeCard);
 
 
 }
--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -1,22 +1,188 @@
+
+
 "use strict"
 
 window.onload = () => {
 
-    /*
-        You can remove the following console.log() lines.
-        They are here to verify that we have access to the data
-        The data script files are located in the scripts/data directory
-    */
+    initLocationselect();
 
-    //log the locationsArray to the console (scripts/data/locationData.js)
-    console.log(locationsArray)
+    initTypeSelect();
 
-    //log the parkTypesArray to the console (scripts/data/parkTypeData.js)
-    console.log(parkTypesArray)
+    let selectByLocation = document.querySelector("#selectLocation");
 
-    //log the nationalParksArray to the console (scripts/data/nationalParkData.js)
-    console.log(nationalParksArray)
+    let selectByType = document.querySelector("#selectType");
 
+    selectByLocation.addEventListener("change", getInfoOffList);
+
+    selectByType.addEventListener("change", gettypeOffList);
+
+}
+function getInfoOffList(event) {
+
+    let selectedState = event.target.value;
+
+    let matchingActivities = nationalParksArray.filter((park) => {
+        return park.State === selectedState;
+
+
+
+    })
+    
+    let tableBody = document.querySelector("#infoTableBody");
+    
+    tableBody.innerHTML = "";
+
+    matchingActivities.forEach((park) => {
+
+        buildTableRow(tableBody, park);
+
+
+    })
+
+    function buildTableRow(tableBody, data) {
+
+        let newRow = tableBody.insertRow(-1);
+
+        let cell1 = newRow.insertCell(0);
+   
+        cell1.innerHTML = data.LocationID
+
+        let cell2 = newRow.insertCell(1);
+        cell2.innerHTML = data.LocationName
+
+        let cell3 = newRow.insertCell(2);
+        cell3.innerHTML = `${data.Address}, ${data.City}, ${data.State}, ${data.ZipCode}`
+
+        let cell4 = newRow.insertCell(3)
+        cell4.innerHTML = `Phone: ${data.Phone}, Fax: ${data.Fax}`
+
+        let cell5 = newRow.insertCell(4)
+        if (!data.Visit) {
+            cell5.innerHTML = "N/A"
+        } else {
+            cell5.innerHTML = data.Visit;
+        }
+    }
+
+}
+function initLocationselect() {
+
+    let selectByLocation = document.querySelector("#selectLocation");
+    
+    let defaultOption = document.createElement("option");
+
+    defaultOption.value = ""
+
+    defaultOption.textContent = "Choose Location";
+
+    selectByLocation.appendChild(defaultOption);
+
+    locationsArray.forEach((state) => {
+
+        let newOption = document.createElement("option");
+
+        newOption.value = state;
+
+        newOption.textContent = state;
+
+        selectByLocation.appendChild(newOption);
+
+
+    })
 
 
 }
+
+function matchByStateName(nationalParksArray, state) {
+
+    let matching = [];
+   
+    let numItems = nationalParksArray.length;
+
+    for (let i = 0; i < numItems; i++) {
+        if (nationalParksArray[i].State === state) {
+            matching.push(nationalParksArray[i]);
+        }
+    }
+
+    return matching;
+}
+
+
+function initTypeSelect() {
+
+    let selectByType = document.querySelector("#selectType");
+   
+    let defaultOption = document.createElement("option");
+
+    defaultOption.value = ""
+
+    defaultOption.textContent = "Choose Type";
+
+    selectByType.appendChild(defaultOption);
+
+    parkTypesArray.forEach((park) => {
+
+        let newOption = document.createElement("option");
+
+        newOption.value = park;
+
+        newOption.textContent = park;
+
+        selectByType.appendChild(newOption);
+
+
+    })
+}
+function gettypeOffList(event) {
+
+    let selectedType = event.target.value;
+
+    let matchingTypes = nationalParksArray.filter((type) => {
+
+        for (let i = 0; i < parkTypesArray.length; i++) {
+            if (type.LocationName.indexOf(selectedType) !== -1) {
+
+                return true
+            }
+            return false
+        }
+
+
+
+    })
+
+    let tableBody = document.querySelector("#infoTableBody");
+   
+    tableBody.innerHTML = "";
+
+    matchingTypes.forEach((type) => {
+
+        buildTableRow(tableBody, type);
+
+    })
+}
+    function buildTableRow(tableBody, chosen) {
+
+        let newRow = tableBody.insertRow(-1);
+
+        let cell1 = newRow.insertCell(0);
+    
+        cell1.innerHTML = chosen.LocationID
+
+        let cell2 = newRow.insertCell(1);
+        cell2.innerHTML = chosen.LocationName
+
+        let cell3 = newRow.insertCell(2);
+        cell3.innerHTML = `${chosen.Address}, ${chosen.City}, ${chosen.State}, ${chosen.ZipCode}`
+
+        let cell4 = newRow.insertCell(3)
+        cell4.innerHTML = `Phone: ${chosen.Phone}, Fax: ${chosen.Fax}`
+
+        let cell5 = newRow.insertCell(4)
+        if (!chosen.Visit) {
+            cell5.innerHTML = "N/A"
+        } else {
+            cell5.innerHTML = chosen.Visit;
+        }
+    }
